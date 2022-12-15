f4ffffe08caf1bd73c118114e74c16435b971373
bethany <bethanylynnpro@gmail.com>
2022-12-14 18:05:55 -0800

    updated index variables

--- produce_summary.py
+++ produce_summary.py
@@ -5,36 +5,39 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
-
+    count = words[1]
+    amount = words[2]
+# changed the index of each variable to the corresponding word
     print(f"Delivered {count} {melon}s for total of ${amount}")
 the_file.close()
+# "Delivered 18 Scaly Bark Watermelons for total of $72.00" is the goal
+# will need a function (def)
+# the problem seems to be that the melon, count, and amount variables
+# are all using the same index
+
+# print("Day 2")
+# the_file = open("um-deliveries-day-2.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
 
 
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# print("Day 3")
+# the_file = open("um-deliveries-day-3.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
