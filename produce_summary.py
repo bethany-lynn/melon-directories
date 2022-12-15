8e953d4bf9944f68bc2a788344fb655c2fb917b1
bethany <bethanylynnpro@gmail.com>
2022-12-14 18:27:46 -0800

    finished melons?

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,52 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-# changed the index of each variable to the corresponding word
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
 # "Delivered 18 Scaly Bark Watermelons for total of $72.00" is the goal
 # will need a function (def)
 # the problem seems to be that the melon, count, and amount variables
 # are all using the same index
 
-# print("Day 2")
-# the_file = open("um-deliveries-day-2.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
 
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
+def melon_count(day, path):
 
-#     print(f"Delivered {count} {melon}s for total of ${amount}")
-# the_file.close()
+    print("Day 1")
+    the_file = open("um-deliveries-day-1.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+    # changed the index of each variable to the corresponding word
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
 
 
-# print("Day 3")
-# the_file = open("um-deliveries-day-3.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
+    print("Day 2")
+    the_file = open("um-deliveries-day-2.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-#     print(f"Delivered {count} {melon}s for total of ${amount}")
-# the_file.close()
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+
+    print("Day 3")
+    the_file = open("um-deliveries-day-3.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+melon_count(1, "um-deliveries-day-1.txt")
+melon_count(2, "um-deliveries-day-2.txt")
+melon_count(3, "um-deliveries-day-3.txt")
